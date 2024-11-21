58e54cc1d8c74dcd80b5a08e359b36fb66179c5f
Pedro Júnior <pedro_alv12@hotmail.com>
2024-11-21 18:46:08 -0300

    Criando função pra limpar e formatar o input name do modalEdit

--- src/components/FormModalEdit/careForm/SanitizeData.js
+++ src/components/FormModalEdit/careForm/SanitizeData.js
@@ -1,14 +1,27 @@
 /* eslint-disable no-unused-vars */
 import DOMPurify from 'dompurify';
 
-const sanitizeName = (name) => {
-  const eachLetter = name.split(' ')[0].split('');
-  let sanitizedName = [];
-  eachLetter.map((item) => {
-   
+const formatFirstName = (name) => {
+  const sanitizedInput = DOMPurify.sanitize(name);
+  const nameCharacters = sanitizedInput.split(' ')[0].split('');
+  let formattedName = [];
+
+  nameCharacters.map((char, index) => {
+    if (index === 0) {
+      formattedName.push(char.toUpperCase());
+    } else {
+      formattedName.push(char.toLowerCase());
+    }
+  });
+
+  return formattedName.join('');
 };
 
 export default function SanitizeDataModalEdit(data) {
-  const cleanName = sanitizeName(data.nome);
-  return cleanName;
+  const sanitizedData = {
+    firstName: formatFirstName(data.nome),
+  };
+  console.log(sanitizedData);
+
+  return sanitizedData;
 }
